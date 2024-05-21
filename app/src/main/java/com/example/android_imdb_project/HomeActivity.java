9951e1af02c7a23c8e32680bd056aa0afc3d1ed1
Alaa Sweed <67858042+alaasw1@users.noreply.github.com>
2024-05-22 00:36:25 +0300

    fixed the toolbar and navigation bar

--- app/src/main/java/com/example/android_imdb_project/HomeActivity.java
+++ app/src/main/java/com/example/android_imdb_project/HomeActivity.java
@@ -39,10 +39,11 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return WindowInsetsCompat.CONSUMED;
         });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -133,6 +134,4 @@ public class HomeActivity extends AppCompatActivity {
         movie.put("photoUrl", photoUrl);
         return movie;
     }
-
-
 }
